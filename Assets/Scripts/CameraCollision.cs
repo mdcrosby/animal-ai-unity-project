5905d7cfc8b57db5fe31cff1128945d37d6133bf
Matthew Crosby <m.crosby@imperial.ac.uk>
2021-04-19 12:11:29 +0100

    Add old prefabs

---
+++ Assets/Scripts/CameraCollision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDistance = 1.0f;
+    public float maxDistance = 4.0f;
+    public float smooth = 10.0f;
+    Vector3 dollyDir;
+    public Vector3 dollyDistAdjusted;
+    public float distance;
+
+    void Awake()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            distance = Mathf.Clamp(hit.distance * 0.9f, minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject followObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = followObj.transform.position;
+        transform.rotation = followObj.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followObj.transform.position;
+        transform.rotation = followObj.transform.rotation;
+    }
+}
