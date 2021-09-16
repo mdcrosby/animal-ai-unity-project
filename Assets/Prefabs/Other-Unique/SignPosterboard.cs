5ed1e4cdb1afca9b762a0268dd69869c43f28977
Joel Holmes <48151621+thanksphil@users.noreply.github.com>
2021-09-16 15:04:22 +0100

    SignPosterboard new u-turn arrow, colour overriding and configuration array
    SignPosterboard new u-turn arrow, colour overriding and configuration array

--- Assets/Prefabs/Other-Unique/SignPosterboard.cs
+++ Assets/Prefabs/Other-Unique/SignPosterboard.cs
@@ -8,8 +8,11 @@ public class SignPosterboard : Prefab
     private Material _symbolMat;
     public string selectedSymbolName;
 
-    // Treat the following arrays as if a dictionary := {symbolNames[0]:textures[0], symbolNames[1]:textures[1], ...} so is serializable
-    public string[] symbolNames; public Texture[] textures;
+    // Treat the following arrays as if a dictionary := {symbolNames[0]:(textures[0],colours[0]), symbolNames[1]:(textures[1],colours[1]), ...} so is serializable
+    public string[] symbolNames; public Texture[] textures; public Color[] colours;
+    private int texIndex;
+    public bool useDefaultColourArray;
+    public Color assignedColourOverride;
 
     void Awake()
     {
@@ -18,14 +21,32 @@ public class SignPosterboard : Prefab
         _symbolMat = this.gameObject.GetComponent<MeshRenderer>().materials[2];
         if (!_symbolMat.name.Contains("symbol")) { Debug.Log("WARNING: a SignPosterboard may not have found the correct symbol material!!"); }
         // sets texture to show correct chosen symbol according to symbol name provided
-        _symbolMat.SetTexture("_BaseMap", getTextureBySymbolName(selectedSymbolName));
+        texIndex = Array.IndexOf(symbolNames, selectedSymbolName);
+
+        if (useDefaultColourArray) {
+            KeyValuePair<Texture, Color> texture_colour_pair = getTextureAndColourByIndex(texIndex);
+            _symbolMat.SetTexture("_BaseMap", texture_colour_pair.Key);
+            _symbolMat.color = texture_colour_pair.Value;
+        }
+        else {
+            Texture texture = getTextureByIndex(texIndex);
+            _symbolMat.SetTexture("_BaseMap", texture);
+            Debug.Log("assignedColourOverride: " + assignedColourOverride);
+            _symbolMat.color = assignedColourOverride;
+        }
 
     }
 
-    private Texture getTextureBySymbolName(string S) {
-        int index = Array.IndexOf(symbolNames, S);
-        // index==0 is default empty transparent texture, so use this if invalid symbol name entered
+    private Texture getTextureByIndex(int index) {
         if (index == -1) { Debug.Log("WARNING: a SignPosterboard has not been given a valid symbol name! Defaulting to empty texture..."); }
-        return ((index!=-1) ? textures[index] : textures[0]);
+        index = (index >= 0 && index < textures.Length) ? index : 0;
+        return textures[index];
+    }
+
+    private KeyValuePair<Texture, Color> getTextureAndColourByIndex(int index) {
+        if (index == -1) { Debug.Log("WARNING: a SignPosterboard has not been given a valid symbol name! Defaulting to empty texture..."); }
+        index = (index >= 0 && index < symbolNames.Length) ? index : 0;
+        Debug.Log("colours[index]: " + colours[index]);
+        return new KeyValuePair<Texture, Color>(textures[index], colours[index]);
     }
 }
