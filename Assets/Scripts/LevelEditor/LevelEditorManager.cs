1f291487e56e0df8b38c56ecdc4b3b31f3622b79
Joel Holmes <48151621+thanksphil@users.noreply.github.com>
2022-02-05 22:12:26 +0000

    training agent removed from arena for level editing
    - LevelEditorManager now removes training agent from instantiated AAI3Arena at Awake() stage
    - rest of functionality working as expected (e.g. camera controls from prev. commit)

--- Assets/Scripts/LevelEditor/LevelEditorManager.cs
+++ Assets/Scripts/LevelEditor/LevelEditorManager.cs
@@ -1,4 +1,5 @@
 using ArenasParameters;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,12 +21,23 @@ public class LevelEditorManager : MonoBehaviour
     {
         _arenasConfigurations = new ArenasConfigurations();
         InstantiateArena();
+        AlterArenaForLevelEditor();
+        // AlterX() must be called in Awake() so changes are made before first frame
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AlterArenaForLevelEditor()
+    {
+        if (_instantiatedArena == null) { throw new Exception("No arena instantiated... can't alter for level editing!!"); }
+        else {
+            // find and destroy the training agent that comes with the Arena packed scene
+            Destroy(_instantiatedArena.transform.Find("AAI3Agent").gameObject);
+        }
     }
 
     private void InstantiateArena()
